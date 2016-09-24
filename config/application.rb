2c6554d53b6d7cedc2abcae0cc8a9aa4f5c78d60
Kandis <kandistag@gmail.com>
2016-09-24 16:01:21 -0700

    updated bootstrap upvote method implemented

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,6 @@
 require_relative 'boot'
 
 require 'rails/all'
-require 'bootstrap-sass'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.

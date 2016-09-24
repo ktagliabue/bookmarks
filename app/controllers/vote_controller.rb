51197f57c71e44cc5d9623e48bdfe6ce419aa28c
Kandis <kandistag@gmail.com>
2016-09-24 16:34:59 -0700

    upvoting fully working

--- app/controllers/vote_controller.rb
+++ app/controllers/vote_controller.rb
@@ -1,2 +1,12 @@
 class VoteController < ApplicationController
+  def create
+    if current_user.votes.where(email_submission: params[:email_submission_id]).empty?
+      @vote = Vote.new
+      @vote.email_submission = EmailSubmission.find(params[:email_submission_id])
+      @vote.user = current_user
+      @vote.value = 1
+      @vote.save
+    end
+    redirect_to :back
+  end
 end
